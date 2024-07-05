61ed9db926f91564627243b940b48b2a3e9ca821
Rojas-Andres <andresfelipe200004@gmail.com>
2024-07-04 23:11:12 -0500

    feat: add initial project

---
+++ src/authentication/apps.py
@@ -0,0 +1,12 @@
+"""
+File to configure the authentication app.
+"""
+from django.apps import AppConfig
+
+
+class AuthenticationConfig(AppConfig):
+    """
+    Class to configure the authentication app.
+    """
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "authentication"
